bf64db69243cec4e4cc28ac06b28f0d40e95968f
Tricia Amheiser <tricia@taarts.net>
2023-01-19 21:09:39 -0500

    started hard coding site

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,5 +1,31 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World</div>
+  return (
+    <div>
+      <div className="content">
+        <div className="section">
+          <header>
+            <nav>
+              <p>welcome</p>
+            </nav>
+          </header>
+          <h1 className="front-page">LOREM IPSUM DOLOR</h1>
+          <main>
+            <section className="a">
+              <div className="aa">
+                everything in it's right place
+                {/* <div className="centered color-teal"> */}
+                {/* <p className="pale-text">start</p> */}
+                {/* </div> */}
+              </div>
+            </section>
+            <section className="b">
+              <div className="bb">everything in it's right place</div>
+            </section>
+          </main>
+        </div>
+      </div>
+    </div>
+  )
 }
